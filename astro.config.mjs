95a4ad4cfb7fd71866fe6f413c44edcf69727d85
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-10-08 18:30:18 +0700

    feat: add robots.txt and sitemap for seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,17 @@
 
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://citra-anugerah.vercel.app",
   output: "static",
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [sitemap(), react()],
 });
