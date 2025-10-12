e88a44afc74907f8b974870541db096df372c938
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-10-13 01:34:45 +0700

    feat: update base url for api base instance

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import { env } from "@/config/env";
 import axios from "axios";
 
 export const apiBaseInstance = axios.create({
-  baseURL: env.API_BASE_URL,
+  baseURL: `${env.API_BASE_URL}/api`,
 });
 
 apiBaseInstance.interceptors.response.use(
