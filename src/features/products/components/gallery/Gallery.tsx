0434eda84d9b29825d2c4a7d060710553aa2c1e4
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-10-20 03:28:58 +0700

    feat: only showing products products gallery if there are products at least one

--- src/features/products/components/gallery/Gallery.tsx
+++ src/features/products/components/gallery/Gallery.tsx
@@ -4,13 +4,14 @@ import type { ProductType } from "@/types/models/product-type";
 export const ProductGallery = ({ products }: { products: ProductType[] }) => {
   return (
     <section className="grid gap-4 sm:grid-cols-2 md:grid-cols-3">
-      {products.map((product, index) => (
-        <ProductCard
-          key={index}
-          image={product.image.formats.medium.url}
-          category={product.category}
-        />
-      ))}
+      {products.length > 0 &&
+        products.map((product, index) => (
+          <ProductCard
+            key={index}
+            image={product.image.formats.medium.url}
+            category={product.category}
+          />
+        ))}
     </section>
   );
 };
