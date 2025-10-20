f1d2246dd8be2367b6746c28372764636d3ccdb1
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-10-20 17:19:37 +0700

    feat: filter products based on current active category

--- src/features/products/components/gallery/Gallery.tsx
+++ src/features/products/components/gallery/Gallery.tsx
@@ -1,11 +1,21 @@
+import { useMemo } from "react";
+import { useProductCategory } from "../../context/ProductCategoryContext";
 import { ProductCard } from "./ProductCard";
 import type { ProductType } from "@/types/models/product-type";
 
 export const ProductGallery = ({ products }: { products: ProductType[] }) => {
+  const { activeCategory } = useProductCategory();
+
+  const filteredProducts = useMemo(() => {
+    return activeCategory === "all"
+      ? products
+      : products.filter((product) => product.category === activeCategory);
+  }, [activeCategory, products]);
+
   return (
     <section className="grid gap-4 sm:grid-cols-2 md:grid-cols-3">
-      {products.length > 0 &&
-        products.map((product, index) => (
+      {filteredProducts.length > 0 &&
+        filteredProducts.map((product, index) => (
           <ProductCard
             key={index}
             image={product.image.formats.medium.url}
