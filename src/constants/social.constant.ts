bd1a6f9e8332cea38a66a3d39cd7a8b541f785c7
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-11-15 16:26:00 +0700

    feat: Location component for showing location with displaying map, location url based on google maps

--- src/constants/social.constant.ts
+++ src/constants/social.constant.ts
@@ -11,4 +11,6 @@ export const SOCIAL_MEDIA = Object.freeze({
   facebook: "https://www.facebook.com/Citra Anugerah/",
   whatsapp: "https://wa.me/6281233303635",
   email: "citraanugerah@gmail.com",
+  location:
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.618652850655!2d110.40511!3d-7.797068!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a57fb5d5d5d5d%3A0x5d5d5d5d5d5d5d5d!2sYogyakarta!5e0!3m2!1sid!2sid!4v1234567890",
 } as const);
