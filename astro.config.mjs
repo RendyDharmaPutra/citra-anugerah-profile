a7bc795ee0c57c88e3c6f46aae3cf34d9511cc6e
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-11-21 13:55:51 +0700

    chore: add vercel adapter, change output to server for accepting ssr option for rendering pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
+import vercel from "@astrojs/vercel";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://citra-anugerah.vercel.app",
-  output: "static",
+  output: "server",
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
